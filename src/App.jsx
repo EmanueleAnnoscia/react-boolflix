c74ef2f5906f6d49afd8aa2935f5a4dc49c6bf18
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-06-17 12:58:20 +0200

    feat card series wip

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,24 @@ function App() {
     // chiamata axios films
     axios.get("https://api.themoviedb.org/3/search/movie?api_key=5142f8b5902e824e62a1db4ec19840e6&query=ciao&language=it-IT")
       .then((response) => {
-        console.log("Movies:", response.data);
-        setMovies(response.data);
+        console.log("Movies:", response.data.results);
+        setMovies(response.data.results);
       });
 
     //chiamata axios series
     axios.get("https://api.themoviedb.org/3/search/tv?api_key=5142f8b5902e824e62a1db4ec19840e6&query=ciao&language=it-IT")
       .then((response) => {
-        console.log("Series:", response.data);
-        setSeries(response.data);
+        console.log("Series:", response.data.results);
+        setSeries(response.data.results);
       });
-  }, []);
+  }, [qui bisogna mettere al cambio di stato del bottone ovvero cliccato si attiva lo use effect]);
 
+//  chiavi dei film
   const { id, original_title, title, original_language, vote_average } = movies
+// chiavi delle serie
+const {id, original_language, original_name, vote_average,}=series
 
+  const combinedResult[...movies, ...series]
 
   return (
     <>
@@ -38,7 +42,10 @@ function App() {
         <nav className="navbar bg-body-tertiary">
           <div className="container-fluid">
             <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" 
+              value = {filterTitle}
+              onChange={(e) => setFilterTitle(e.target.value)}
+              />
               <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
           </div>
@@ -54,11 +61,11 @@ function App() {
                   style={{ objectFit: "cover", height: "300px" }}
                 />
                 <div className="card-body">
-                  <h5 className="card-title">{film.title}</h5>
+                  <h5 className="card-title">{movie.title}</h5>
                   <p className="card-text">
-                    <strong>Titolo originale:</strong> {film.original_title}<br />
-                    <strong>Lingua:</strong> {film.original_language}<br />
-                    <strong>Voto:</strong> {film.vote_average}
+                    <strong>Titolo originale:</strong> {movie.original_title}<br />
+                    <strong>Lingua:</strong> {movie.original_language}<br />
+                    <strong>Voto:</strong> {movie.vote_average}
                   </p>
                 </div>
               </div>
