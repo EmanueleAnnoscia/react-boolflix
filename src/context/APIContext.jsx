124a212ff26c439ab07e811170dc085e05f94112
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-06-18 12:39:26 +0200

    tentativo di migliorare l'axios

--- src/context/APIContext.jsx
+++ src/context/APIContext.jsx
@@ -1,6 +1,9 @@
 import { createContext, useState } from "react";
 import axios from "axios";
 
+
+
+
 const GlobalContext = createContext();
 
 const GlobalContextProvider = ({ children }) => {
@@ -8,17 +11,18 @@ const GlobalContextProvider = ({ children }) => {
   const [series, setSeries] = useState([]);
   const [filterTitle, setFilterTitle] = useState("breaking");
 
-  const api_key = "5142f8b5902e824e62a1db4ec19840e6";
+  const api_key = import.meta.env.VITE_API_KEY ;
+  const api_url = import.meta.env.VITE_API_URL;
 
   const handleSearch = (event) => {
     event.preventDefault();
 
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${filterTitle}&language=it-IT`)
+    axios.get(`${api_url}/search/movie?api_key=${api_key}&query=${filterTitle}&language=it-IT`)
       .then((response) => {
         setMovies(response.data.results);
       });
 
-    axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${filterTitle}&language=it-IT`)
+    axios.get(`${api_url}/search/tv?api_key=${api_key}&query=${filterTitle}&language=it-IT`)
       .then((response) => {
         setSeries(response.data.results);
       });
